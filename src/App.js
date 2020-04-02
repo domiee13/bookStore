4277be5872e18ba49e045f67e3e664a9dedef893
Domiee <dungngocmd@gmail.com>
2020-04-02 12:59:33 +0700

    axios module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import logo from './logo.svg';
 import './App.css';
 import TopMenu from './components/TopMenu';
@@ -24,6 +23,8 @@ function App() {
             <h2>THIS IS HOME PAGE</h2>
           </Route>
           <Route path='/product' component = {Product}></Route>
+          <Route path='/login' ><h1>Login page</h1></Route>
+          <Route path='/signup'><h1>Signup page</h1></Route>
         </Switch>
       </div>
     </Router>
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Container, Row, Col
   } from 'reactstrap';
   
 export default class Product extends Component{
-   
+   constructor(props){
+     super(props);
+     this.state = {
+       products: []
+     }
+   }
+   componentDidMount(){
+    axios.get("https://localhost:5000/api/products").then(res=>{
+      console.log(res); 
+      console.log("Bug cai dmm");
+    // this.setState({
+      //   products: res.data
+      // });
+    });
+    console.log("Component did mount");
+  }
+  render(){
+    return(
+      <h2>This is Product site</h2>
+    );
+  }
 }
+
